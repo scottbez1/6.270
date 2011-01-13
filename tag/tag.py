7e1cf72874163dbca2f5353e083d25fd9cfe79fa
Peter Iannucci <iannucci@mit.edu>
2011-01-12 19:15:16 -0500

    Fixed stupid svg bug.

--- tag/tag.py
+++ tag/tag.py
@@ -51,27 +51,28 @@ f.write(a[0] % {'width':totalwidth, 'height':totalheight})
 
 def svgRect(**kwargs):
     global id
-    style = {}
+    style = {} if 'style' not in kwargs else kwargs['style']
     if 'fill' in kwargs:
-        style['fill'] = kwargs['fill']
+        del kwargs['fill']
     if 'stroke' in kwargs:
         style['stroke'] = kwargs['stroke']
-    args = {'style':';'.join(['%s:%s'%(k,v) for k,v in style.iteritems()]),'id':'r%d' % id}
-    args.update(kwargs)
-    tag = '  <rect ' + ' '.join(['%s="%s"'%(k,v) for k,v in args.iteritems()]) + ' />\n'
+        del kwargs['stroke']
+    kwargs['style'] = ';'.join(['%s:%s'%(k,v) for k,v in style.iteritems()])
+    kwargs['id'] = 'r%d' % id
+    tag = '  <rect ' + ' '.join(['%s="%s"'%(k,v) for k,v in kwargs.iteritems()]) + ' />\n'
     id += 1
     f.write(tag)
 
-svgRect(x=0, y=0, width=totalwidth, height=totalheight, fill=black)
+svgRect(x=0, y=0, width=totalwidth, height=totalheight, style=dict(fill=black))
 svgRect(x=blackborder, y=blackborder, width=whiteborder*2+cols*square,
-        height=whiteborder*2+rows*square, fill=white)
+        height=whiteborder*2+rows*square, style=dict(fill=white))
 
 for i in range(rows):
     for j in range(cols):
         if not bits[i*rows+j]:
             svgRect(x=origin+j*square, y=origin+i*square, width=square,
-                    height=square, fill=black, stroke=black,
-                    **{'stroke-width':'.5pt'})
+                    height=square, style=dict(fill=black, stroke=black,
+                    **{'stroke-width':'.5pt'}))
 
 f.write(a[1])
 f.close()
---
+++ vision/game.h
@@ -0,0 +1,22 @@
+#ifndef _GAME_H_INC_
+#define _GAME_H_INC_
+
+#define MATCH_LEN_SECONDS 60
+
+#define MATCH_ENDED 0
+#define MATCH_RUNNING 1
+
+//size of space to project onto
+#define X_MIN -2048
+#define X_MAX 2047
+
+#define Y_MIN -2048
+#define Y_MAX 2047
+
+#define FOOT 512.0
+
+#define GOAL_TOLERANCE 50
+
+#define MAX_ROBOT_ID 12288
+
+#endif

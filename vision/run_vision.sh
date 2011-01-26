971c1fc51ec4678142914058613f1a642dcfc2e9
sixtwoseventy <sixtwoseventy@sixtwoseventy-desktop.(none)>
2011-01-26 14:58:11 -0500

    Added $@ to run_vision

--- vision/run_vision.sh
+++ vision/run_vision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /bin/bash -c "guvcview --control_only --device=/dev/video$1 -l ~/6.270/vision/uvcSettings.gpfl & "; sleep 1 && killall guvcview
-make && ./vision $1 $2
+make && ./vision $@
 
